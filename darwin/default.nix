bab08c856848302a887a909b8da5149ecacacc57
rgarza <rene@r3n3.co>
2022-02-13 18:24:29 -0600

    clean

--- darwin/default.nix
+++ darwin/default.nix
@@ -1,22 +1,11 @@
 { pkgs, lib, ... }:
 
 {
-#   imports = [
-#     # Minimal config of Nix related options and shells
-#     ./bootstrap.nix
-
-#     # Other nix-darwin configuration
-#     ./homebrew.nix
-#     ./defaults.nix
-#   ];
-
-  # Networking
   networking.dns = [
     "1.1.1.1"
     "8.8.8.8"
   ];
 imports = [
-    # Minimal config of Nix related options and shells
     ./bootstrap.nix
   ];
   # Add ability to used TouchID for sudo authentication
